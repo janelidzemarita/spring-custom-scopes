fc0ffb6764e831dc08768592d0e8b775f74b5736
M.J <janelidze.marita@kiu.edu.ge>
2023-10-09 19:34:04 +0400

    Initial Commit - Solution

--- src/main/java/com/rd/epam/autotasks/scopes/config/ThreadScopeConfig.java
+++ src/main/java/com/rd/epam/autotasks/scopes/config/ThreadScopeConfig.java
@@ -1,9 +1,66 @@
 package com.rd.epam.autotasks.scopes.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.Scope;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.NamedThreadLocal;
+
+import java.util.*;
 
 @Configuration
 public class ThreadScopeConfig {
+    @Bean
+    public static BeanFactoryPostProcessor beanFactoryPostProcessor() {
+        return new BeanFactoryPostProcessor() {
+            class ThreadScope implements Scope {
+                private final ThreadLocal<Map<String, Object>> threadScope =
+                        new NamedThreadLocal<>(ThreadScope.class.getName()) {
+                            @Override
+                            protected Map<String, Object> initialValue() {
+                                return new HashMap<>();
+                            }
+                        };
 
+                @Override
+                public Object get(String name, ObjectFactory<?> objectFactory) {
+                    Map<String, Object> scope = this.threadScope.get();
+                    Object object = scope.get(name);
+                    if (object == null) {
+                        object = objectFactory.getObject();
+                        scope.put(name, object);
+                    }
+                    return object;
+                }
 
+                @Override
+                public Object remove(String s) {
+                    return null;  //Not needed for this assignment
+                }
+
+                @Override
+                public void registerDestructionCallback(String s, Runnable runnable) {
+                    //Not needed for this assignment
+                }
+
+                @Override
+                public Object resolveContextualObject(String s) {
+                    return null;  //Not needed for this assignment
+                }
+
+                @Override
+                public String getConversationId() {
+                    return null;  //Not needed for this assignment
+                }
+            }
+
+            @Override
+            public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+                configurableListableBeanFactory.registerScope("thread", new ThreadScope());
+            }
+        };
+    }
 }
